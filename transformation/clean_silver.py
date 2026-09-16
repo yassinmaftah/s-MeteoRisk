0855be6de10cb10001b0f7808ec84e09e8786aea
yassinmaftah <yassinmaftah06@gmail.com>
2026-09-16 22:00:20 +0100

    add function to calculate weather risk scores

--- transformation/clean_silver.py
+++ transformation/clean_silver.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 # from transformation.clean_silver import json_to_dataframe
 from json_to_df import json_to_dataframe
 
@@ -20,12 +21,32 @@ def clean_data(df):
     df.drop_duplicates(inplace=True)
     cities_df = pd.read_csv('../bronze/ma.csv')
     df = pd.merge(df, cities_df[['city', 'lat', 'lng']], on='city', how='left')
+    df = calculate_risk_features(df)
     df.to_csv('../silver/df_silver.csv', index=False)
     return df
 
 def fix_mission_day(date):
     return date + pd.Timedelta(days=1)
 
+def calculate_risk_features(df):
+    wind_conditions = [
+        df['wind_gusts_10m_max'] > 85,
+        df['wind_gusts_10m_max'] > 60,
+        df['wind_gusts_10m_max'] > 40
+    ]
+    wind_scores = [50, 30, 15]
+    df['risk_wind'] = np.select(wind_conditions, wind_scores, default=0)
+    df['risk_rain'] = df['precipitation_probability_max'] * 0.3
+
+    temp_condition = (df['temperature_2m_max'] > 40) | (df['temperature_2m_min'] < 2)
+    df['risk_temp'] = np.where(temp_condition, 20, 0)
+
+    df['risk_score_total'] = df['risk_wind'] + df['risk_rain'] + df['risk_temp']
+
+    df['risk_score_total'] = np.where(df['risk_score_total'] > 100, 100, df['risk_score_total'])
+    all_risk_cols = ['risk_wind', 'risk_rain', 'risk_temp', 'risk_score_total']
+    df[all_risk_cols] = df[all_risk_cols].round(2)
+    return df
 
 df = json_to_dataframe('../bronze/data01.json')
 cleaned_df = clean_data(df)
